99ed12f581e66c7e59444be5468c1aa55bd8114b
Tarun Nahak <tarunnahak25@gmail.com>
2023-05-11 01:36:25 +0530

    added the View image screen

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 export default function App() {
-  return <WelcomeScreen />;
+  return (
+    // <WelcomeScreen />
+    <ViewImageScreen />
+  );
 }
