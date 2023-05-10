e3dfd695abc568a8e2b4ee6684719ee3a50f1ccf
Tarun Nahak <tarunnahak25@gmail.com>
2023-05-11 02:08:21 +0530

    added the Apptext component for encapsulated text

--- App.js
+++ App.js
@@ -2,10 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import AppText from "./app/components/AppText";
 
 export default function App() {
   return (
     // <WelcomeScreen />
-    <ViewImageScreen />
+    // <ViewImageScreen />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <AppText>Hi there how are you?</AppText>
+    </View>
   );
 }
