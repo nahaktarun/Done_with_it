acc113b0923c1cc0bed8c13f12811ad6d6820fb5
Tarun Nahak <tarunnahak25@gmail.com>
2023-05-15 01:47:08 +0530

    ADDED - ListItem component and refactored listing details screen

--- app/components/ListingDetailsScreen.js
+++ app/components/ListingDetailsScreen.js
@@ -2,6 +2,9 @@ import React from "react";
 import { View, StyleSheet, Image } from "react-native";
 import AppText from "./AppText";
 import colors from "../config/colors";
+
+import ListItem from "./ListItem";
+
 export default function ListingDetailsScreen() {
   return (
     <View>
@@ -9,6 +12,13 @@ export default function ListingDetailsScreen() {
       <View style={styles.detailsContainer}>
         <AppText style={styles.title}>White Chair for sale</AppText>
         <AppText style={styles.subTitle}>$100</AppText>
+        <View style={styles.userContainer}>
+          <ListItem
+            image={require("../assets/icon.png")}
+            title={"Tarun Nahak"}
+            subTitle={"6 Listings"}
+          />
+        </View>
       </View>
     </View>
   );
@@ -31,5 +41,8 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     fontWeight: 500,
+  },
+  userContainer: {
+    marginVertical: 40,
   },
 });
