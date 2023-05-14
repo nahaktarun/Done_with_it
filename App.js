273fd3b1e018962826e58261b886746dc9fe5b67
Tarun Nahak <tarunnahak25@gmail.com>
2023-05-15 01:56:07 +0530

    REFACTOR - viewimagescreen with icons

--- App.js
+++ App.js
@@ -19,6 +19,7 @@ export default function App() {
     //     image={require("./app/assets/chair.jpg")}
     //   />
     // </View>
-    <ListingDetailsScreen />
+    // <ListingDetailsScreen />
+    <ViewImageScreen />
   );
 }
