1eba2502dcab7191c29060a1c97056c095237494
Tarun Nahak <tarunnahak25@gmail.com>
2023-05-15 00:34:02 +0530

    ADDED - Button Component

--- App.js
+++ App.js
@@ -3,13 +3,18 @@ import { View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import AppText from "./app/components/AppText";
+import AppButton from "./app/components/AppButton";
 
 export default function App() {
   return (
     // <WelcomeScreen />
     // <ViewImageScreen />
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <AppText>I Love react Native</AppText>
+      {/* <AppText>I Love react Native</AppText> */}
+      <AppButton
+        title="Login"
+        onPress={() => console.log("Tapped")}
+      ></AppButton>
     </View>
   );
 }
