cfad0a383270519f2153bbf6c8b6b2d65227acfc
Tarun Nahak <tarunnahak25@gmail.com>
2023-05-15 01:02:07 +0530

    ADDED - card component

--- App.js
+++ App.js
@@ -4,17 +4,19 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import AppText from "./app/components/AppText";
 import AppButton from "./app/components/AppButton";
+import Card from "./app/components/Card";
 
 export default function App() {
   return (
-    <WelcomeScreen />
+    // <WelcomeScreen />
     // <ViewImageScreen />
-    // <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-    //   {/* <AppText>I Love react Native</AppText> */}
-    //   <AppButton
-    //     title="Login"
-    //     onPress={() => console.log("Tapped")}
-    //   ></AppButton>
-    // </View>
+
+    <View style={{ backgroundColor: "#f8f4f4", padding: 20, paddingTop: 100 }}>
+      <Card
+        title="Red Jacket for sale"
+        subTitle="$100"
+        image={require("./app/assets/chair.jpg")}
+      />
+    </View>
   );
 }
